337414448fe08060f6ac76c6341685f56699697a
alex <alexbr1208@gmail.com>
2024-10-21 09:52:39 -0400

    -- added functions

--- main.cpp
+++ main.cpp
@@ -1,16 +1,31 @@
+// libaraies
 #include <iostream>
 #include <cmath>
 
+// namespaces
 using namespace std;
+
+int add (int a, int b) {
+    return a + b;
+}
+int sub (int a, int b) {
+    return a - b;
+}
+int multiply (int a, int b) {
+    return a * b;
+}
+int divide (int a, int b) {
+    return a / b;
+}
 
 int main() {
     //variables
-    double firstVariable {};
-    char operation {};
-    char yesNo {};
+    double firstVariable  {};
+    char operation        {};
+    char yesNo            {};
     double secondVariable {};
+    bool inputsCompleted  {};
 
-    bool inputsCompleted {};
     while (not inputsCompleted) {
         cout << "Enter first variable, then enter your operator, lastly your last variable: ( x + y )" << '\n';
         cin >> firstVariable;
@@ -20,24 +35,25 @@ int main() {
         double result {};
         switch (operation) {
             case '+':
-                result = firstVariable + secondVariable;
+                result = add(firstVariable, secondVariable);
             break;
             case '-':
-                result = firstVariable - secondVariable;
+                result = sub(firstVariable, secondVariable);
             break;
             case '/':
-                result = firstVariable / secondVariable;
+                result = divide(firstVariable, secondVariable);
             break;
             case '*':
-                result = firstVariable * secondVariable;
+                result = multiply(firstVariable, secondVariable);
             break;
             default:
+                cout << "Invalid operation or missing variable!" << '\n';
                 return 1;
 
         }
 
 
-        cout << "Result: " << result << endl;
+        cout << "Result: " << result << '\n';
         cout << "Would you like another calculation? (y/n) " << '\n';
         cin >> yesNo;
 

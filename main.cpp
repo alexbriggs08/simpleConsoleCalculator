73a6243d6683e0b5efd43ebd740adb6333b155f4
alex <alexbr1208@gmail.com>
2024-10-21 08:52:44 -0400

    changed readablity

--- main.cpp
+++ main.cpp
@@ -9,13 +9,11 @@ int main() {
     char operation {};
     char yesNo {};
     double secondVariable {};
-    double result {};
-    bool inputsCompleted {};
 
     while (not inputsCompleted) {
         cout << "Enter first variable: " << '\n';
         cin >> firstVariable;
-        cout << "Enter your operation: (+, -, /, *) " << '\n';
+        cout << "Enter your operator: (+, -, /, *) " << '\n';
         cin >> operation;
         cout << "Enter second variable: " << '\n';
         cin >> secondVariable;
@@ -38,10 +36,12 @@ int main() {
 
         }
 
+        double result {};
         cout << "Result: " << result << endl;
         cout << "Would you like another calculation? (y/n) " << '\n';
         cin >> yesNo;
 
+        bool inputsCompleted {};
         if (yesNo == 'y' || yesNo == 'Y') {
             inputsCompleted = false;
         } else {
